a9d756ee38e6ce2b86027bd8b50ba94676f84715
anushkaagarwal9089 <73290702+anushkaagarwal9089@users.noreply.github.com>
2023-08-12 16:22:44 +0530

    Update data_collection.py

--- data_collection.py
+++ data_collection.py
@@ -1,26 +1,25 @@
 import yfinance as yf
 import pandas as pd
 
-# Define the stock symbol and date range
+# Defining the stock symbol and date range
 stock_symbol = 'AAPL'
 start_date = '2020-01-01'
 end_date = '2023-01-01'
 
-# Fetch historical stock price data
+# Fetching the historical stock price data
 stock_data = yf.download(stock_symbol, start=start_date, end=end_date)
 
-# Calculate daily returns
+# Calculating the daily returns
 stock_data['Daily_Return'] = stock_data['Adj Close'].pct_change()
 
 # Drop the first row with NaN values
 stock_data.dropna(inplace=True)
 
-# Assuming you have other features as well (you can customize this part)
 # Example: Moving Averages
 stock_data['5_day_MA'] = stock_data['Adj Close'].rolling(window=5).mean()
 stock_data['20_day_MA'] = stock_data['Adj Close'].rolling(window=20).mean()
 
-# Drop unnecessary columns (customize based on your features)
+# Droppikng the unnecessary columns
 columns_to_drop = ['Open', 'High', 'Low', 'Close', 'Volume', 'Adj Close']
 stock_data.drop(columns=columns_to_drop, inplace=True)
 
