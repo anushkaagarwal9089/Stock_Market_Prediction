9f974f9fc946125585aeb9bab5d8ce60910c5c08
anushkaagarwal9089 <73290702+anushkaagarwal9089@users.noreply.github.com>
2023-08-12 16:24:53 +0530

    Update model_training.py

--- model_training.py
+++ model_training.py
@@ -4,18 +4,18 @@ from sklearn.preprocessing import MinMaxScaler
 import tensorflow as tf
 import joblib
 
-# Load preprocessed data (replace with your data path)
+# Loading the preprocessed data
 data_path = 'preprocessed_data.csv'
 stock_data = pd.read_csv(data_path)
 
-# Extract the target variable (Daily_Return)
+# Extracting the target variable (Daily_Return)
 y = stock_data['Daily_Return'].values
 
-# Normalize the target variable
+# Normalizing the target variable
 scaler = MinMaxScaler()
 y_scaled = scaler.fit_transform(y.reshape(-1, 1))
 
-# Define a function to create input sequences and labels for the LSTM model
+# Defining a function to create input sequences and labels for the LSTM model
 def create_sequences(data, sequence_length):
     X = []
     y = []
@@ -24,26 +24,26 @@ def create_sequences(data, sequence_length):
         y.append(data[i+sequence_length])
     return np.array(X), np.array(y)
 
-# Create sequences and labels
+# Creating sequences and labels
 sequence_length = 10
 X, y = create_sequences(y_scaled, sequence_length)
 
-# Split data into training and testing sets
+# Splitting the data into training and testing sets
 split_ratio = 0.8
 split_index = int(split_ratio * len(X))
 X_train, X_test = X[:split_index], X[split_index:]
 y_train, y_test = y[:split_index], y[split_index:]
 
-# Build the LSTM model
+# Building the LSTM model
 model = tf.keras.Sequential([tf.keras.layers.LSTM(50, activation='relu', input_shape=(sequence_length, 1)), tf.keras.layers.Dense(1)])
 
-# Compile the model
+# Compiling the model
 model.compile(optimizer='adam', loss='mse')
 
-# Train the model
+# Training the model
 model.fit(X_train, y_train, epochs=50, batch_size=32)
 
-# Evaluate the model on the test set
+# Evaluating the model on the test set
 loss = model.evaluate(X_test, y_test)
 print(f"Test Loss: {loss}")
 
